d903fb64c23f23c11c34e1efd683f57bf7c6d95c
daodaoq <2090953265@qq.com>
2025-01-18 16:58:35 +0800

    完成负责人数据的获取

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -9,6 +9,13 @@
       </el-form-item>
       <el-form-item prop="managerId" label="部门负责人">
         <el-select v-model="formData.managerId" placeholder="请选择负责人" style="width: 80%" size="mini">
+          <!-- 下拉选项 循环 负责人数据 label表示显示的字段 value 存储字段 -->
+          <el-option
+            v-for="item in managerList"
+            :key="item.id"
+            :label="item.username"
+            :value="item.id"
+          />
         </el-select>
       </el-form-item>
       <el-form-item prop="introduce" label="部门介绍">
@@ -27,7 +34,7 @@
   </el-dialog>
 </template>
 <script>
-import { getDepartment } from '@/api/department'
+import { getDepartment, getManagerList } from '@/api/department'
 
 export default {
   name: 'AddDept',
@@ -39,6 +46,7 @@ export default {
   },
   data() {
     return {
+      managerList: [], // 存储负责人列表
       formData: {
         code: '', // 部门编码
         introduce: '', // 部门介绍
@@ -93,9 +101,15 @@ export default {
       }
     }
   },
+  created() {
+    this.getManagerList()
+  },
   methods: {
     close() {
       this.$emit('update:showDialog', false)
+    },
+    async getManagerList() {
+      this.managerList = await getManagerList()
     }
   }
 }
